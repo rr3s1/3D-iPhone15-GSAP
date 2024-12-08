33d10255bdae5c2a831a840e5432cb4bdd817e55
aessary <raressilviulazar@gmail.com>
2024-12-08 09:11:59 +0000

    Navbar p1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Higlights"
+
+const  App = () => {
+  
+  return (
+    <main className="bg-black">
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+    </main>
+  )
+}
+
+export default App
