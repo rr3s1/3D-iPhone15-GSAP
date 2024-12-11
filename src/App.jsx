b489604a21de5b9d5cc7c0ec49a0706cc4c01c7e
aessary <raressilviulazar@gmail.com>
2024-12-11 09:19:06 +0000

    Features section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Higlights"
 import VideoCarousel from "./components/VideoCarousel";
 import Model from "./components/Model";
+import Features from "./components/Features";
 
 const  App = () => {
   
@@ -13,6 +14,7 @@ const  App = () => {
         <Highlights/>
         <VideoCarousel />
         <Model />
+        <Features />
     </main>
   )
 }
